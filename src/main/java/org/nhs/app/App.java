9bff9ddb62fa7a6a82a02c814896c2c3ff1f013c
shritapalla <1118394@apps.nsd.org>
2024-10-09 04:24:04 +0000

    Done

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,48 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+public class App {
+     public static void main(String [] args){
+    /*for(int number = 1; number<= 5; number++){
+        for(int counter =1; counter <= 4; counter++){
+            System.out.print(number);
+        }
+        System.out.println();
+    }*/
 
-package org.nhs.app;
 
-// Your code goes here
+    /*for(int number = 1; number<= 5; number++){
+            for(int counter = 1; counter<= number; counter++){
+                System.out.print(number);
+            }
+            System.out.println();
+        }*/
+
+    for(int i = 1; i<= 5; i++){
+        for(int x = i; x <= 4; x++){
+            System.out.print(".");
+        }
+        for(int x =1; x<=i; x++){
+            System.out.print(i);
+        }
+        System.out.println();
+    }
+    System.out.println();
+
+    for(int i = 1; i<= 5; i++){
+        for(int x = i; x <= 4; x++){
+            System.out.print(".");
+        }
+        System.out.print(i);
+        for(int x =1; x<i; x++){
+            System.out.print(".");
+        }
+        System.out.println();
+    }
+    System.out.println();
+    
+
+
+     }
+    }
+
+
+
+
